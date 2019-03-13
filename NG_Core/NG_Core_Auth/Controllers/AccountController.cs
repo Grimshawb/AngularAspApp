12969493fa966fe8956d161d37805a5d5b84c04c
brad <grimshawb@gmail.com>
2019-03-13 19:13:29 -0400

    Add account controller and user registration

---
+++ NG_Core/NG_Core_Auth/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        // Inject these services into controller
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signManager;
+
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        // This would be the action name in the address bar. e.g., ex.com/api/account/register
+        // so we use the action attribute.
+        // FromBody attribute says that our information is going to come from the body of our front-end app
+        [HttpPost("action")]
+        public async Task<IActionResult> Register( [FromBody] RegisterViewModel formData) 
+        {
+            List<String> errorList = new List<string>();
+            var user = new IdentityUser
+            { 
+                Email = formData.Email,
+                UserName = formData.UserName,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formData.Password);
+
+            if(result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                // Send confirmation email
+
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registration Successful" });
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
+        }
+
+    }
+}
--- NG_Core/NG_Core_Auth/Startup.cs
+++ NG_Core/NG_Core_Auth/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NG_Core_Auth.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace NG_Core_Auth
 {
@@ -41,6 +43,21 @@ namespace NG_Core_Auth
 
             // Connect to DB
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Declare use of Identity Framework for AccountController
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.User.RequireUniqueEmail = true;
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
