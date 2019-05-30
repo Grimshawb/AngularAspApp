a1dcb350969fa001e08dfaaa3ed9c8e9a1db60e9
brad <grimshawb@gmail.com>
2019-05-30 19:34:18 -0400

    Images added and some CSS
    CSS for home component. Images added. App font set.

--- NG_Core/NG_Core_Auth/ClientApp/src/app/app-routing.module.ts
+++ NG_Core/NG_Core_Auth/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot([
+    {path: "home", component: HomeComponent},
+    {path: "", component: HomeComponent, pathMatch: 'full'},
+    {path: "**", redirectTo: '/home'},
+    {path: "login", component: LoginComponent, pathMatch: 'full'},
+    {path: "register", component: RegisterComponent, pathMatch: 'full'},
+    {path: "products", component: ProductsComponent, pathMatch: 'full'}
+        ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
